2017bab1751981a6bac7a9349a87c381a96e1eea
金子健哉 <kaneko@queserser-j.co.jp>
2018-02-28 14:44:52 +0900

    .

--- main.py
+++ main.py
@@ -25,8 +25,10 @@ def get_last_twitter_id():
 
     return last_twitter_id[0]
 
-twitter_id = get_last_twitter_id();
-profile = ""
+# TODO クラスにする
+last_twitter_id = get_last_twitter_id();
+user_infomation = ""
+user_info_list = []
 
 # Twitter情報
 auth = twitter.OAuth(consumer_key=CK,
@@ -36,13 +38,42 @@ auth = twitter.OAuth(consumer_key=CK,
 
 t = twitter.Twitter(auth=auth)
 # フォロワーリスト取得
-follower_lists = t.followers.ids(user_id=twitter_id, count=5)
+follower_lists = t.followers.ids(user_id=last_twitter_id, count=5)
 # 取得したidのフォロワーが5つ取得できるかどうか
 if len(follower_lists["ids"]) >= 5:
     # SQLiteに入れる
-    for follower_list in follower_lists["ids"]:
-        profile = t.users.show(user_id=follower_list)
-        print(profile)
+    for i, follower_list in enumerate(follower_lists["ids"]):
+        user_infomation = t.users.show(user_id=follower_list)
+        temp_list = []
+        temp_list.append(user_infomation["id"])
+        temp_list.append(user_infomation["screen_name"])
+        temp_list.append(user_infomation["name"])
+        temp_list.append(user_infomation["description"])
+        temp_list.append(user_infomation["location"])
+        temp_list.append("unilaterally")
+        user_info_list.append(temp_list)
 else:
     # id列から-1をする
     pass
+
+
+
+with closing(sqlite3.connect(DBNAME)) as conn:
+    c = conn.cursor()
+# SQLiteに入れる
+sql = '''
+    INSERT INTO twitter_lists(
+        twitter_id,
+        user_id,
+        user_name,
+        profile,
+        location,
+        status
+    )
+    VALUES (?, ?, ?, ?, ?, ?)
+'''
+c.executemany(sql, user_info_list)
+conn.commit()
+conn.close()
+
+
